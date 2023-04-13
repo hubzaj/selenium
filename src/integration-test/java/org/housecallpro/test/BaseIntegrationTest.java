48e4ec285ce8cbdf5eeff367a3a1053ab2c8b579
Hubert Zajac <hubertzajac6@gmail.com>
2023-04-13 23:10:48 +0200

    add Junit5 support

---
+++ src/integration-test/java/org/housecallpro/test/BaseIntegrationTest.java
@@ -0,0 +1,13 @@
+package org.housecallpro.test;
+
+import org.housecallpro.page.PageInitializer;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class BaseIntegrationTest implements PageInitializer {
+
+    @BeforeAll
+    static void setupBrowser() {
+
+    }
+
+}
---
+++ src/integration-test/java/org/housecallpro/test/ExampleIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.housecallpro.test;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    void shouldCreateNewJob() {
+
+    }
+
+}

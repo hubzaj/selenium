b22920940692fc09f194aaee986aa069dfd80851
Hubert Zajac <hubertzajac6@gmail.com>
2023-04-14 16:18:01 +0200

    resource manager

--- src/integration-test/java/org/housecallpro/browser/BrowserType.java
+++ src/integration-test/java/org/housecallpro/browser/BrowserType.java
@@ -1,6 +1,22 @@
 package org.housecallpro.browser;
 
+import java.util.Arrays;
+
 public enum BrowserType {
     CHROME,
-    FIREFOX
+    CHROME_DOCKER,
+    SAFARI;
+
+    public static BrowserType getBrowser(String name) {
+        try {
+            return valueOf(name.toUpperCase());
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException(
+                    String.format("[%s] browser is not supported. Please use one of [%s]",
+                            name,
+                            Arrays.stream(values()).map(Enum::toString).toList())
+            );
+        }
+    }
+
 }
--- src/integration-test/java/org/housecallpro/test/BaseIntegrationTest.java
+++ src/integration-test/java/org/housecallpro/test/BaseIntegrationTest.java
@@ -1,12 +1,15 @@
 package org.housecallpro.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.housecallpro.browser.BrowserType;
 import org.housecallpro.page.PageInitializer;
+import org.housecallpro.resource.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,9 +24,27 @@ public abstract class BaseIntegrationTest implements PageInitializer {
 
     @BeforeAll
     void setupBrowser() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        logger.info("[%s] browser has been opened with success");
+        BrowserType browser = Configuration.getBrowserConfig().getBrowser();
+        switch (browser) {
+            case CHROME_DOCKER -> {
+                if (WebDriverManager.isDockerAvailable()) {
+                    WebDriverManager.chromedriver().browserInDocker().setup();
+                    driver = new ChromeDriver();
+                    logger.info("[{}] browser has been opened with success", browser.name());
+                }
+            }
+            case SAFARI -> {
+                WebDriverManager.safaridriver().setup();
+                driver = new SafariDriver();
+                logger.info("[{}] browser has been opened with success", browser.name());
+            }
+            default -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                logger.info("[{}] browser has been opened with success", browser.name());
+            }
+        }
+
     }
 
     @AfterEach

79edecf92305b5537eb8e45c14506865bd7bc9ad
Hubert Zajac <hubertzajac6@gmail.com>
2023-04-17 09:23:16 +0200

    chrome in docker

--- src/integration-test/java/org/housecallpro/browser/BrowserFactory.java
+++ src/integration-test/java/org/housecallpro/browser/BrowserFactory.java
@@ -13,6 +13,9 @@ import static org.openqa.selenium.PageLoadStrategy.NORMAL;
 @Slf4j
 public class BrowserFactory {
 
+    private static final String OPEN_BROWSER_WITH_SUCCESS_LOG_MESSAGE = "[{}] browser has been opened with success";
+    private static final String HEADLESS_MODE = "--headless=new";
+
     private BrowserFactory() {
     }
 
@@ -20,49 +23,36 @@ public class BrowserFactory {
         return switch (browser) {
             case CHROME -> {
                 WebDriverManager.chromedriver().setup();
-
-                ChromeOptions options = new ChromeOptions();
-                options.setPageLoadStrategy(NORMAL);
-
-                WebDriver driver = new ChromeDriver(options);
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-                driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
-                driver.manage().window().maximize();
-
-                log.info("[{}] browser has been opened with success", browser.name());
+                WebDriver driver = createDefaultChromeDriver();
+                log.info(OPEN_BROWSER_WITH_SUCCESS_LOG_MESSAGE, browser.name());
                 yield driver;
             }
             case CHROME_HEADLESS -> {
                 WebDriverManager.chromedriver().setup();
-
-                ChromeOptions options = new ChromeOptions();
-                options.setPageLoadStrategy(NORMAL);
-                options.addArguments("--headless=new");
-
-                WebDriver driver = new ChromeDriver(options);
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-                driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
-                driver.manage().window().maximize();
-
-                log.info("[{}] browser has been opened with success", browser.name());
+                WebDriver driver = createDefaultChromeDriver(HEADLESS_MODE);
+                log.info(OPEN_BROWSER_WITH_SUCCESS_LOG_MESSAGE, browser.name());
                 yield driver;
             }
             case CHROME_IN_DOCKER -> {
                 WebDriverManager.chromedriver().browserInDocker().create();
-
-                ChromeOptions options = new ChromeOptions();
-                options.setPageLoadStrategy(NORMAL);
-                options.addArguments("--headless=new");
-
-                WebDriver driver = new ChromeDriver(options);
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-                driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
-                driver.manage().window().maximize();
-
-                log.info("[{}] browser has been opened with success", browser.name());
+                WebDriver driver = createDefaultChromeDriver(HEADLESS_MODE);
+                log.info(OPEN_BROWSER_WITH_SUCCESS_LOG_MESSAGE, browser.name());
                 yield driver;
             }
         };
+    }
+
+    private static WebDriver createDefaultChromeDriver(String... arguments) {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(NORMAL);
+        options.addArguments(arguments);
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+
+        return driver;
     }
 
 }
